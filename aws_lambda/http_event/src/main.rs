825dd4d13204a5728989a3efe14035fb598987e3
lep511 <estebanpbuday@gmail.com>
2024-10-22 17:21:39 -0300

    update

---
+++ aws_lambda/http_event/src/main.rs
@@ -0,0 +1,23 @@
+use aws_lambda_events::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
+use http::HeaderMap;
+use lambda_runtime::{service_fn, Error, LambdaEvent};
+
+async fn handler(
+    _event: LambdaEvent<ApiGatewayProxyRequest>,
+) -> Result<ApiGatewayProxyResponse, Error> {
+    let mut headers = HeaderMap::new();
+    headers.insert("content-type", "text/html".parse().unwrap());
+    let resp = ApiGatewayProxyResponse {
+        status_code: 200,
+        multi_value_headers: headers.clone(),
+        is_base64_encoded: false,
+        body: Some("Hello AWS Lambda HTTP request".into()),
+        headers,
+    };
+    Ok(resp)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    lambda_runtime::run(service_fn(handler)).await
+}

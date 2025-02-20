9b138d7b4993fb72dc30fb38ca5fe91df9c2a243
Esteban Perez <estebanpbuday@gmail.com>
2025-02-20 16:19:01 +0000

    update

--- aws_lambda/medical-data/src/http_handler.rs
+++ aws_lambda/medical-data/src/http_handler.rs
@@ -1,12 +1,14 @@
-use lambda_http::{Body, Error, Request, RequestExt, Response};
-use lambda_http::tracing::info;
-use serde_json::{Value, json};
+use lambda_http::{Body, Error, Request, Response};
+use lambda_http::tracing::{error, info};
+use crate::libs::CDSHooksResponse;
+use serde_json::json;
 
 pub(crate) async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     // Extract some useful information from the request
-    info!("Event: {:?}", event);
+    // info!("Event: {:?}", event);
+    info!("Body: {:?}", event.body());
     let path_fm = event.uri().path();
-    let method = event.method();
+    // let method = event.method();
 
     let path: String = path_fm.rsplitn(3, '/')
         .take(2)
@@ -19,7 +21,7 @@ pub(crate) async fn function_handler(event: Request) -> Result<Response<Body>, E
     match path.as_str() {
         "cds-services/0001" => {
             info!("Services path cds-services-0001");
-            handle_patient_view()
+            handle_patient_view(event.body())
         }
         _ => {
             handle_discovery()
@@ -46,14 +48,79 @@ fn handle_discovery() -> Result<Response<Body>, Error> {
     create_response(discovery_response)
 }
 
-fn handle_patient_view() -> Result<Response<Body>, Error> {
+fn handle_patient_view(hook_data: &Body) -> Result<Response<Body>, Error> {
+    
+    let body_str = match hook_data {
+        Body::Text(body) => body,
+        _ => {
+            error!("[E0993] Body is not text.");
+            return handle_error();
+        }
+    };
+
+    let cds_response: CDSHooksResponse = match serde_json::from_str(body_str) {
+        Ok(data) => data,
+        Err(error) => {
+            error!("[E0994] Error parsing body: {:?}", error);
+            return handle_error();
+        }
+    };
+
+    let names: (String, String) = match extract_patient_name(&cds_response) {
+        Some(names) => names,
+        None => {
+            error!("[E0995] Error extracting patient name.");
+            return handle_error();
+        }
+    };
+
+    let greeting = format!(
+        "Hello, {} {}!", 
+        names.0,
+        names.1
+    );
+
+    let patient_view = json!({ 
+        "cards": [
+            {
+                "summary": greeting,
+                "indicator": "info",
+                "source": {
+                    "label": "test service"
+                }
+            }
+        ]
+    });
+
+    create_response(patient_view)
+}
+
+fn extract_patient_name(response: &CDSHooksResponse) -> Option<(String, String)> {
+    if let Some(prefetch) = &response.prefetch {
+        if let Some(patient) = &prefetch.patient {
+            if let Some(names) = &patient.name {
+                // Assuming the first name in the list is the primary name
+                if let Some(human_name) = names.get(0) {
+                    let family_name = human_name.family.clone().unwrap_or_default();
+                    let given_names = human_name.given.clone().unwrap_or_default();
+                    let given_name = given_names.join(" ");
+                    return Some((given_name, family_name));
+                }
+            }
+        }
+    }
+    None
+}
+
+fn handle_error() -> Result<Response<Body>, Error> {
+       
     let patient_view = json!({ 
         "cards": [
             {
                 "summary": "patient-view",
                 "indicator": "info",
                 "source": {
-                    "label": "test service"
+                    "label": "No event"
                 }
             }
         ]
